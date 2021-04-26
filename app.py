b443956720a77ccd4bc0e07a6887a9844a64bcc1
sha1207 <83126866+sha1207@users.noreply.github.com>
2021-04-26 19:39:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = str(name)+"loves shahid"
+	new_name = str(name)+" loves shahid"
 	return render_template('index.html', name=name)
 
 
